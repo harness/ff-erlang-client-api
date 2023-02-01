8fe170ec8b707f45715015aed2b4317582361a0f
Jake Morrison <jake@cogini.com>
2023-02-01 09:59:14 -0600

    Fix types

--- src/cfapi_metrics_api.erl
+++ src/cfapi_metrics_api.erl
@@ -3,21 +3,21 @@
 -export([post_metrics/3, post_metrics/4]).
 
 % @doc Send metrics to the Analytics server.
--spec post_metrics(ctx:ctx(), binary(), binary()) ->
-  {ok, [], cfapi_utils:response_info()}
+-spec post_metrics(ctx:ctx(), map(), binary()) ->
+  {ok, list(), cfapi_utils:response_info()}
   | {ok, hackney:client_ref()}
   | {error, term(), cfapi_utils:response_info()}.
-post_metrics(Ctx, Cluster, Environment) -> post_metrics(Ctx, Cluster, Environment, #{}).
+post_metrics(Ctx, QueryOpts, Environment) -> post_metrics(Ctx, QueryOpts, Environment, #{}).
 
--spec post_metrics(ctx:ctx(), binary(), binary(), maps:map()) ->
-  {ok, [], cfapi_utils:response_info()}
+-spec post_metrics(ctx:ctx(), map(), binary(), maps:map()) ->
+  {ok, list(), cfapi_utils:response_info()}
   | {ok, hackney:client_ref()}
   | {error, term(), cfapi_utils:response_info()}.
-post_metrics(Ctx, Cluster, Environment, Opts) ->
+post_metrics(Ctx, QueryOpts, Environment, Opts) ->
   Params = maps:get(params, Opts, #{}),
   Cfg = maps:get(cfg, Opts, application:get_env(kuberl, config, #{})),
   Path = [<<"/metrics/">>, Environment],
-  QS = cfapi_utils:optional_params([cluster], Cluster),
+  QS = cfapi_utils:optional_params([cluster], QueryOpts),
   ContentTypeHeader = cfapi_utils:select_header_content_type([<<"application/json">>]),
   HackneyOpts = maps:get(hackney_opts, Opts, []),
   cfapi_utils:request(Ctx, post, Path, QS, ContentTypeHeader, Params, HackneyOpts, Cfg).
